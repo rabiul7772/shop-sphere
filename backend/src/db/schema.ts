0867e7c5223a520374fb1392ade7538ad46663e4
rabiul7772 <rabiulakand777@gmail.com>
2026-01-22 01:08:12 +0600

    fix: updatedAt field for prodcuts & users schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -9,7 +9,10 @@ export const users = pgTable('users', {
   name: text('name'),
   imageUrl: text('image_url'),
   createdAt: timestamp('created_at', { mode: 'date' }).notNull().defaultNow(),
-  updatedAt: timestamp('updated_at', { mode: 'date' }).notNull().defaultNow()
+  updatedAt: timestamp('updated_at', { mode: 'date' })
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date())
 });
 
 export const products = pgTable('products', {
@@ -21,7 +24,10 @@ export const products = pgTable('products', {
     .notNull()
     .references(() => users.id, { onDelete: 'cascade' }),
   createdAt: timestamp('created_at', { mode: 'date' }).notNull().defaultNow(),
-  updatedAt: timestamp('updated_at', { mode: 'date' }).notNull().defaultNow()
+  updatedAt: timestamp('updated_at', { mode: 'date' })
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date())
 });
 
 export const comments = pgTable('comments', {
